ef297f8374944d4f82dda648186efe32d8c30c87
vishnu <vishnu.21792@gmail.com>
2022-08-08 23:09:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var vehicles = ["van", "car","bus","train"]
+var person = prompt("please enter name")
+// if(person.length < 14){
+//     alert("please enter name greater than 14 characters " )
+// }else{
+//     alert("greater than 14 characters " )
+// }
+alert(person.indexOf("vardhan"))
+function myFunction(){
+    let pas =document.getElementById("passangers").value
+
+   const vechile = vechileType(pas)
+   document.getElementById("demo").innerHTML="for "+pas+" passangers vechile suggested is "+vechile
+}    
+
+function vechileType(noofpassangers){
+    if(noofpassangers <= 2){
+        return "car"
+    }else if(5 > noofpassangers > 2){
+        return "van"
+    }else if(24 > noofpassangers > 5){
+        return "bus"
+    }else {
+        return "train"
+    }
+}
